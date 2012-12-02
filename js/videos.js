c23682adc7de9062f074d453d978d5613100eb16
Emmanuel Bautista <emmanuel.bautista@gmail.com>
2012-12-03 07:27:49 +0800

    modfied video event placements

--- js/videos.js
+++ js/videos.js
@@ -7,22 +7,103 @@
 _vid  = function(v, config){ //pass the id
     
     this.video = document.getElementById(v);
+    var initialState = this.video.getAttribute('style');
     this.config = config;
     this._config = config;
-    this.layout = {x:1,y:2};
-    this.resetConfig = function(){
-        this.config = this._config;
+    this.layout = {
+        x:1,
+        y:2
+    };
+    this.getOriginalConfig = function(){
+        return this.config;
+        
+    };
+    
+    this.getVideoElement = function(){
+        return this.video;
     };
     this.initialize = function(){
         //console.log("initialize");
         var config = this.config;
-        
+        var _this = this;
         this.video.addEventListener("timeupdate", function(){
-            var currentNode = null;
             
+        }, true);
+        
+        //  any video error will fail with message 
+        this.video.addEventListener("error", function (err) {
+            this.errMessage(err);
+        }, true);
+
+        // content has loaded, display buttons and set up events
+        this.video.addEventListener("canplay", function () {
+        
+            }, false);
+
+        //  display video duration when available
+        this.video.addEventListener("loadedmetadata", function () {
+            var vLength = this.duration.toFixed(1);
+            console.log("loadedmetadata");
+        }, false);
+
+        //  display the current and remaining times
+        this.video.addEventListener("timeupdate", function () {
+            //  Current time  
+            console.log("timeupdate");
+        
+        }, false);
+        //  paused and playing events to control buttons
+        this.video.addEventListener("pause", function () {
+            console.log("pause");
+        }, false);
+
+        this.video.addEventListener("playing", function () {
+            console.log("playing");
+        }, false);
+
+        this.video.addEventListener("volumechange", function () {
+            if (this.muted) {
+            // if muted, show mute image
             
-            for(item=0; item< config.length; item++){                
-                if(currentNode == null && config[item]['on'] == Math.floor(this.currentTime)){
+            } else {
+            // if not muted, show not muted image
+            
+            }
+        }, false);
+        //  Download and playback status events.
+        this.video.addEventListener("loadstart", function () {
+            console.log("loadstart");
+        }, false);
+        this.video.addEventListener("loadeddata", function () {
+            console.log("loadeddata");
+        }, false);
+
+        this.video.addEventListener("ended", function () {
+            console.log("ended");
+        }, false);
+
+        this.video.addEventListener("emptied", function () {
+            console.log("emptied");
+        }, false);
+
+        this.video.addEventListener("stalled", function () {
+            console.log("stalled");
+        }, false);
+    
+        this.video.addEventListener("waiting", function () {
+            console.log("waiting");
+        }, false);
+    
+        this.video.addEventListener("progress", function () {
+            console.log("progress");
+            if(0 == Math.floor(this.currentTime)){
+                for(var item=0; item< config.length; item++){                                           
+                    config[item].isExecuted = false;
+                }
+            }
+            
+            for(var item=0; item< config.length; item++){                
+                if(config[item]['on'] == Math.floor(this.currentTime)){
                     if(config[item].isExecuted == undefined || config[item].isExecuted == false){
                         config[item]['do'].apply(this, arguments);
                         config[item].isExecuted = true;
@@ -31,7 +112,10 @@ _vid  = function(v, config){ //pass the id
                 }
             }
             
-            
+        }, false);
+    
+        this.video.addEventListener("canplaythrough", function () {
+            console.log("canplaythrough");
         }, false);
     }
     
@@ -39,89 +123,12 @@ _vid  = function(v, config){ //pass the id
         this.initialize();
     }
     
-    this.getVideoElement = function(){
-        return this.video;
-    }
     
-    //  any video error will fail with message 
-    this.video.addEventListener("error", function (err) {
-        this.errMessage(err);
-    }, true);
-
-    // content has loaded, display buttons and set up events
-    this.video.addEventListener("canplay", function () {
-        
-    }, false);
-
-    //  display video duration when available
-    this.video.addEventListener("loadedmetadata", function () {
-        var vLength = this.duration.toFixed(1);
-        console.log("loadedmetadata");
-    }, false);
-
-    //  display the current and remaining times
-    this.video.addEventListener("timeupdate", function () {
-        //  Current time  
-        console.log("timeupdate");
-        
-    }, false);
-    //  paused and playing events to control buttons
-    this.video.addEventListener("pause", function () {
-        console.log("pause");
-    }, false);
-
-    this.video.addEventListener("playing", function () {
-        console.log("playing");
-    }, false);
-
-    this.video.addEventListener("volumechange", function () {
-        if (this.muted) {
-            // if muted, show mute image
-            
-        } else {
-            // if not muted, show not muted image
-            
-        }
-    }, false);
-    //  Download and playback status events.
-    this.video.addEventListener("loadstart", function () {
-        console.log("loadstart");
-    }, false);
-    this.video.addEventListener("loadeddata", function () {
-        console.log("loadeddata");
-    }, false);
-
-    this.video.addEventListener("ended", function () {
-        console.log("ended");
-    }, false);
-
-    this.video.addEventListener("emptied", function () {
-        console.log("emptied");
-    }, false);
-
-    this.video.addEventListener("stalled", function () {
-        console.log("stalled");
-    }, false);
-    
-    this.video.addEventListener("waiting", function () {
-        console.log("waiting");
-    }, false);
-    
-    this.video.addEventListener("progress", function () {
-        console.log("progress");
-        if(0 == Math.floor(this.currentTime)){
-            resetConfig();
-        }
-    }, false);
-    
-    this.video.addEventListener("canplaythrough", function () {
-        console.log("canplaythrough");
-    }, false);
     
     this.errorMessage = function(msg) {
-                // displays an error message for 5 seconds then clears it
-                alert(msg);
-            }
+        // displays an error message for 5 seconds then clears it
+        alert(msg);
+    }
     
     this.initialize();
 }
